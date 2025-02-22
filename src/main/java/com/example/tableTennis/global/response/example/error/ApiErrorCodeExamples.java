0d46166ebccb7c8e76d0ef48b09db3c33b69713e
0702Yoon <gkstkddbs99@naver.com>
2025-02-22 15:22:26 +0900

    Feat: 스웨거 예외 상황을 위한 애너테이션 커스텀

---
+++ src/main/java/com/example/tableTennis/global/response/example/error/ApiErrorCodeExamples.java
@@ -0,0 +1,14 @@
+package com.example.tableTennis.global.response.example.error;
+
+import com.example.tableTennis.global.response.ResponseType;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ApiErrorCodeExamples {
+
+    ResponseType[] value();
+}
