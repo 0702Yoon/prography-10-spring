89fcb4a9b22f818526ffd4bd22fcd01cd89fe576
0702Yoon <gkstkddbs99@naver.com>
2025-02-22 15:51:16 +0900

    Test:

---
+++ src/test/java/com/example/tableTennis/TableTennisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tableTennis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableTennisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
